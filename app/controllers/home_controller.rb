d6ffc6a2af4e39aa9da31282a34d3553dd7e9fe1
toannguyenle <toan@me.com>
2015-01-29 18:36:20 -0800

    pal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,14 @@ class HomeController < ApplicationController
     require 'nokogiri'
     require 'open-uri'
     # Get a Nokogiri::HTML::Document for the page we’re interested in...
-    doc = Nokogiri::HTML(open('http://catalog.lapl.org/carlweb/jsp/DoSearch?databaseID=965&initialsearch=true&count=10&finish=search_page.jsp&mode=manual&terms=ruby+on+rails&Search=Search&index=w'))
+    # doc = Nokogiri::HTML(open('http://catalog.lapl.org/carlweb/jsp/DoSearch?databaseID=965&initialsearch=true&count=10&finish=search_page.jsp&mode=manual&terms=ruby+on+rails&Search=Search&index=w'))
+    uber = Nokogiri::HTML(open('http://www.promotioncode.org/socialshare.php?cid=1976700&xid=85318'))
+    doc = Nokogiri::HTML(open('http://www.promotioncode.org/Uber/1976700/'))
 
     doc.css('.titleListTitle').each do |link|
-      puts '#####LINK CONTENT##########'
       puts link.content
     end
-    puts '####DOC##########################'
-    puts doc
-    render html: doc
+    # render html: doc
   end
 end
 
